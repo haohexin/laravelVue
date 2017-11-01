2b1825d09113352de171e21f1ca4154f7430eb8b
haohexin <2554470320@qq.com>
2017-11-01 09:15:11 +0800

    解决跨域问题

--- routes/api.php
+++ routes/api.php
@@ -17,15 +17,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//  文章列表
-Route::get('/posts', function (Request $request) {
-    $data = \App\Post::all();
-    return response()->json($data);
+Route::group(['middleware' => ['cor']], function () {
+    //  文章列表
+    Route::get('/posts', function (Request $request) {
+        $data = \App\Post::all();
+        return response()->json($data);
+    });
+
+    //  文章详情
+    Route::get('/postDetail/{id}', function (Request $request, $id) {
+        $data = \App\Post::find($id);
+        return response()->json($data);
+    });
 });
 
-//  文章详情
-Route::get('/postDetail/{id}', function (Request $request, $id) {
-    $data = \App\Post::find($id);
-    return response()->json($data);
-});
+
 
